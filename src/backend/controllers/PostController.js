9a5bb3ae32b5f8331e87c3f7f078d65ac3b954f0
AtulPant2704 <atulpant1999@gmail.com>
2022-05-13 14:21:13 +0530

    Initial commit

--- src/backend/controllers/PostController.js
+++ src/backend/controllers/PostController.js
@@ -85,6 +85,7 @@ export const createPostHandler = function (schema, request) {
         likeCount: 0,
         likedBy: [],
         dislikedBy: [],
+        comments: [],
       },
       username: user.username,
       createdAt: formatDate(),
@@ -180,7 +181,7 @@ export const likePostHandler = function (schema, request) {
       (currUser) => currUser._id !== user._id
     );
     post.likes.likeCount += 1;
-    post.likes.likedBy.push(user);
+    post.likes.likedBy.push(JSON.parse(JSON.stringify(user)));
     this.db.posts.update({ _id: postId }, { ...post, updatedAt: formatDate() });
     return new Response(201, {}, { posts: this.db.posts });
   } catch (error) {
