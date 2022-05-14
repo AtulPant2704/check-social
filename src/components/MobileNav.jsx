14580dd0dca1c78a044f98da00a6529a8b411383
AtulPant2704 <atulpant1999@gmail.com>
2022-05-14 16:47:20 +0530

    feat: add mobile nav

---
+++ src/components/MobileNav.jsx
@@ -0,0 +1,35 @@
+import { ListItem, UnorderedList, ListIcon } from "@chakra-ui/react";
+import { FaHome, FaUserCircle } from "react-icons/fa";
+import { BsFillBookmarkFill } from "react-icons/bs";
+import { MdOutlineExplore } from "react-icons/md";
+import { IoMdAddCircle } from "react-icons/io";
+
+const MobileNav = ({ onOpen }) => {
+  return (
+    <UnorderedList
+      listStyleType="none"
+      h="50px"
+      display="flex"
+      justifyContent="space-around"
+      alignItems="center"
+    >
+      <ListItem>
+        <ListIcon as={FaHome} fontSize="25px" color="white" />
+      </ListItem>
+      <ListItem cursor="pointer" fontSize="25px" color="white">
+        <ListIcon as={MdOutlineExplore} />
+      </ListItem>
+      <ListItem cursor="pointer" fontSize="25px" color="white" onClick={onOpen}>
+        <ListIcon as={IoMdAddCircle} />
+      </ListItem>
+      <ListItem cursor="pointer" fontSize="25px" color="white">
+        <ListIcon as={BsFillBookmarkFill} />
+      </ListItem>
+      <ListItem cursor="pointer" fontSize="25px" color="white">
+        <ListIcon as={FaUserCircle} />
+      </ListItem>
+    </UnorderedList>
+  );
+};
+
+export { MobileNav };
