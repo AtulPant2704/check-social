a6a31fc170d9ca29b0357658d7b6a2a2e09384ac
AtulPant2704 <atulpant1999@gmail.com>
2022-05-17 08:36:06 +0530

    chore: add dummy users and posts

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -7,98 +7,255 @@ import { formatDate } from "../utils/authUtils";
 
 export const users = [
   {
-    _id: uuid(),
+    _id: "79Gksh9pq7",
     firstName: "Adarsh",
     lastName: "Balika",
     username: "adarshbalika",
     password: "adarshBalika123",
-    bio: "Hello World",
-    bookmarks: [],
-    avatarUrl: "",
-    website: "https://google.com/",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    avatarURL: "",
+    following: [
+      {
+        _id: "79Gk42f9otl",
+        firstName: "Guest",
+        lastName: "User",
+        username: "Guest123",
+        avatarURL: "",
+      },
+      {
+        _id: "79Gksh9otl",
+        firstName: "John",
+        lastName: "Doe",
+        username: "johndoe",
+        avatarURL:
+          "https://res.cloudinary.com/check-social/image/upload/v1652722184/third_mrjqm3.jpg",
+      },
+      {
+        _id: "1T6Be1QpLm",
+        firstName: "Jane",
+        lastName: "Doe",
+        username: "janedoe",
+        avatarURL:
+          "https://res.cloudinary.com/check-social/image/upload/v1652722197/fourth_rgotvm.jpg",
+      },
+    ],
+    followers: [
+      {
+        _id: "79Gksh9otl",
+        firstName: "John",
+        lastName: "Doe",
+        username: "johndoe123",
+        avatarURL:
+          "https://res.cloudinary.com/check-social/image/upload/v1652722184/third_mrjqm3.jpg",
+      },
+
+      {
+        _id: "LCrc9f0Zl0",
+        firstName: "Carl",
+        lastName: "Smith",
+        username: "carlsmith",
+        avatarURL:
+          "https://res.cloudinary.com/check-social/image/upload/v1652722212/first_bvrlba.jpg",
+      },
+    ],
+    bookmarks: [],
+    bio: "I am the first user",
+    portfolio: "https://adarshbalika.netlify.app/",
   },
+  /*****************************************2*******************************************/
   {
-    _id: uuid(),
+    _id: "79Gk42f9otl",
     firstName: "Guest",
     lastName: "User",
     username: "Guest123",
     password: "test123",
-    bio: "Hello World",
-    bookmarks: [],
-    avatarUrl: "",
-    website: "https://google.com/",
     createdAt: formatDate(),
     updatedAt: formatDate(),
-  },
-  {
-    _id: "79Gksh9otl",
-    firstName: "John",
-    lastName: "Doe",
-    username: "johndoe",
-    password: "johndoe123",
-    bio: "Hello World",
+    avatarURL: "",
+    following: [
+      {
+        _id: "79Gksh9pq7",
+        firstName: "Adarsh",
+        lastName: "Balika",
+        username: "adarshbalika",
+        avatarURL: "",
+      },
+      {
+        firstName: "John",
+        lastName: "Doe",
+        username: "johndoe",
+        avatarURL:
+          "https://res.cloudinary.com/check-social/image/upload/v1652722184/third_mrjqm3.jpg",
+      },
+    ],
+    followers: [
+      {
+        _id: "79Gksh9pq7",
+        firstName: "Adarsh",
+        lastName: "Balika",
+        username: "adarshbalika",
+        avatarURL: "",
+      },
+      {
+        firstName: "John",
+        lastName: "Doe",
+        username: "johndoe",
+        avatarURL:
+          "https://res.cloudinary.com/check-social/image/upload/v1652722184/third_mrjqm3.jpg",
+      },
+      {
+        _id: "1T6Be1QpLm",
+        firstName: "Jane",
+        lastName: "Doe",
+        username: "janedoe",
+        avatarURL:
+          "https://res.cloudinary.com/check-social/image/upload/v1652722197/fourth_rgotvm.jpg",
+      },
+    ],
     bookmarks: [],
-    avatarUrl:
-      "https://res.cloudinary.com/check-social/image/upload/v1652722184/third_mrjqm3.jpg",
-    website: "https://google.com/",
-    createdAt: "2022-01-02T10:55:06+05:30",
-    updatedAt: formatDate(),
+    bio: "I am the second user",
+    portfolio: "https://www.google.co.in/",
   },
+  /*******************************************3****************************************************/
   {
     _id: "1T6Be1QpLm",
     firstName: "Jane",
     lastName: "Doe",
     username: "janedoe",
     password: "janedoe123",
-    bio: "Whats in bio?",
-    bookmarks: [],
-    avatarUrl:
-      "https://res.cloudinary.com/check-social/image/upload/v1652722197/fourth_rgotvm.jpg",
-    website: "https://atulpant.netlify.app/",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    avatarURL:
+      "https://res.cloudinary.com/check-social/image/upload/v1652722197/fourth_rgotvm.jpg",
+    followers: [
+      {
+        _id: "79Gksh9pq7",
+        firstName: "Adarsh",
+        lastName: "Balika",
+        username: "adarshbalika",
+        avatarURL: "",
+      },
+      {
+        firstName: "John",
+        lastName: "Doe",
+        username: "johndoe",
+        avatarURL:
+          "https://res.cloudinary.com/check-social/image/upload/v1652722184/third_mrjqm3.jpg",
+      },
+    ],
+    following: [
+      {
+        _id: "79Gksh9pq7",
+        firstName: "Adarsh",
+        lastName: "Balika",
+        username: "adarshbalika",
+        avatarURL: "",
+      },
+      {
+        _id: "LCrc9f0Zl0",
+        firstName: "Carl",
+        lastName: "Smith",
+        username: "carlsmith",
+        avatarURL:
+          "https://res.cloudinary.com/check-social/image/upload/v1652722212/first_bvrlba.jpg",
+      },
+    ],
+    bookmarks: [],
+    bio: "I am third user",
+    portfolio: "https://www.google.co.in/",
   },
+  /**************************************************4**************************************************/
   {
     _id: "LCrc9f0Zl0",
     firstName: "Carl",
     lastName: "Smith",
     username: "carlsmith",
     password: "carlsmith123",
-    bio: "Whats in bio?",
-    avatarUrl:
-      "https://res.cloudinary.com/check-social/image/upload/v1652722212/first_bvrlba.jpg",
-    website: "https://atulpant.netlify.app/",
-    createdAt: "2022-01-03T10:55:06+05:30",
+    createdAt: formatDate(),
     updatedAt: formatDate(),
+    avatarURL:
+      "https://res.cloudinary.com/check-social/image/upload/v1652722212/first_bvrlba.jpg",
+    following: [
+      {
+        _id: "M1NR81Bzlz",
+        firstName: "Clement",
+        lastName: "Maxwell",
+        username: "clement14",
+        avatarURL:
+          "https://res.cloudinary.com/check-social/image/upload/v1652722226/fifth_dg1lz2.jpg",
+      },
+    ],
+    followers: [
+      {
+        _id: "79Gksh9pq7",
+        firstName: "Adarsh",
+        lastName: "Balika",
+        username: "adarshbalika",
+        avatarURL: "",
+      },
+    ],
+    bookmarks: [],
+    bio: "I am fourth user",
+    portfolio: "https://www.google.co.in/",
   },
+  /*******************************************5*******************************************/
   {
     _id: "M1NR81Bzlz",
     firstName: "Clement",
     lastName: "Maxwell",
     username: "clement14",
     password: "clement123",
-    bio: "What's up?",
-    bookmarks: [],
-    avatarUrl:
-      "https://res.cloudinary.com/check-social/image/upload/v1652722226/fifth_dg1lz2.jpg",
-    website: "",
-    createdAt: "2022-01-05T10:55:06+05:30",
+    createdAt: formatDate(),
     updatedAt: formatDate(),
+    avatarURL:
+      "https://res.cloudinary.com/check-social/image/upload/v1652722226/fifth_dg1lz2.jpg",
+    followers: [
+      {
+        firstName: "John",
+        lastName: "Doe",
+        username: "johndoe",
+        avatarURL:
+          "https://res.cloudinary.com/check-social/image/upload/v1652722184/third_mrjqm3.jpg",
+      },
+    ],
+    following: [
+      {
+        _id: "79Gk42f9otl",
+        firstName: "Guest",
+        lastName: "User",
+        username: "Guest123",
+        avatarURL: "",
+      },
+    ],
+    bookmarks: [],
+    bio: "I am fifth user",
+    portfolio: "https://www.google.co.in/",
   },
+  /*********************************************6*****************************************/
   {
     _id: "qq8zWjEeXd",
     firstName: "Marina",
     lastName: "Mogilko",
     username: "marina27",
     password: "marina123",
-    bio: "English Instructor",
-    bookmarks: [],
-    avatarUrl:
-      "https://res.cloudinary.com/check-social/image/upload/v1652722233/second_wmzgil.jpg",
-    website: "",
-    createdAt: "2022-01-06T10:55:06+05:30",
+    createdAt: formatDate(),
     updatedAt: formatDate(),
+    avatarURL:
+      "https://res.cloudinary.com/check-social/image/upload/v1652722233/second_wmzgil.jpg",
+    followers: [
+      {
+        _id: "1T6Be1QpLm",
+        firstName: "Jane",
+        lastName: "Doe",
+        username: "janedoe",
+        avatarURL:
+          "https://res.cloudinary.com/check-social/image/upload/v1652722197/fourth_rgotvm.jpg",
+      },
+    ],
+    following: [],
+    bookmarks: [],
+    bio: "I am sixth user",
+    portfolio: "https://www.google.co.in/",
   },
 ];
