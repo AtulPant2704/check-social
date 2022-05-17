b6359836b748a661a75945c8acefc7af5575f839
AtulPant2704 <atulpant1999@gmail.com>
2022-05-17 09:09:48 +0530

    feat: server call for getting users

--- src/redux/slices/usersSlice.js
+++ src/redux/slices/usersSlice.js
@@ -1,14 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getUsers } from "redux/asyncThunks";
 
 const initialState = {
   users: [],
+  isLoading: false,
 };
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    [getUsers.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getUsers.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.users = action.payload.data.users;
+      console.log(action);
+    },
+    [getUsers.rejected]: (state, action) => {
+      state.isLoading = false;
+      console.error(action.error.message);
+    },
+  },
 });
 
 export const { reducer: usersReducer } = usersSlice;
