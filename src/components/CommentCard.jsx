16a5195438867d8046b1d4e272d1a2ebb27f8d6e
AtulPant2704 <atulpant1999@gmail.com>
2022-05-18 23:04:26 +0530

    feat: delete comment

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,4 +1,5 @@
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import {
   Heading,
@@ -14,79 +15,137 @@ import {
   PopoverBody,
   PopoverArrow,
   PopoverCloseButton,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { FaRegEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import { EditCommentModal } from "./EditCommentModal";
+import { deleteComment } from "redux/asyncThunks";
 
-const CommentCard = ({ comment }) => {
+const CommentCard = ({ comment, postId }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const dispatch = useDispatch();
+  const toast = useToast();
   const navigate = useNavigate();
-  const { user } = useSelector((state) => state.auth);
+  const { user, token } = useSelector((state) => state.auth);
+  const [editedComment, setEditedComment] = useState("");
+  const [commentId, setCommentId] = useState(null);
+
+  const callEditCommentHandler = () => {
+    setCommentId(comment._id);
+    setEditedComment(comment.commentData);
+    onOpen();
+  };
+
+  const deleteCommentHandler = async () => {
+    const response = await dispatch(
+      deleteComment({ postId, commentId: comment._id, token })
+    );
+    if (response?.payload.status === 201) {
+      toast({
+        description: "Post successfully deleted",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        description: `${response.payload.data.errors[0]}`,
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
-    <Flex gap="2" alignItems="center">
-      <Avatar
-        name={comment.firstName + " " + comment.lastName}
-        src={
-          comment.username === user.username
-            ? user.avatarUrl
-            : comment.avatarUrl
-        }
-        size="sm"
-        cursor="pointer"
-        onClick={() => navigate(`/profile/${comment.username}`)}
-      />
-      <Flex justifyContent="space-between" width="100%">
-        <Box>
-          <Heading
-            as="h5"
-            size="sm"
-            cursor="pointer"
-            onClick={() => navigate(`/profile/${comment.username}`)}
-          >
-            {comment.firstName} {comment.lastName}
-          </Heading>
-          <Text>{comment.text}</Text>
-        </Box>
-      </Flex>
-      {/* Edit Comment */}
-      {user.username === comment.username ? (
-        <Popover>
-          <PopoverTrigger>
-            <IconButton
-              icon={<BsThreeDotsVertical />}
-              bgColor="transparent"
-              color="black"
-              size="sm"
-              fontSize="lg"
-              _hover={{
-                bgColor: "transparent",
-              }}
-              _active={{
-                bgColor: "transparent",
-                border: "none",
-              }}
-              _focus={{
-                bgColor: "transparent",
-                border: "none",
-              }}
-            ></IconButton>
-          </PopoverTrigger>
-          <PopoverContent maxW="fit-content">
-            <PopoverArrow />
-            <PopoverCloseButton />
-            <PopoverBody>
-              <Button leftIcon={<FaRegEdit />} variant="ghost" display="block">
-                Edit
-              </Button>
-              <Button leftIcon={<MdDelete />} variant="ghost">
-                Delete
-              </Button>
-            </PopoverBody>
-          </PopoverContent>
-        </Popover>
+    <>
+      {isOpen ? (
+        <EditCommentModal
+          isOpen={isOpen}
+          onClose={onClose}
+          editedComment={editedComment}
+          setEditedComment={setEditedComment}
+          commentId={commentId}
+          setCommentId={setCommentId}
+          postId={postId}
+        />
       ) : null}
-    </Flex>
+      <Flex gap="2" alignItems="center">
+        <Avatar
+          name={comment.firstName + " " + comment.lastName}
+          src={
+            comment.username === user.username
+              ? user.avatarUrl
+              : comment.avatarUrl
+          }
+          size="sm"
+          cursor="pointer"
+          onClick={() => navigate(`/profile/${comment.username}`)}
+        />
+        <Flex justifyContent="space-between" width="100%">
+          <Box>
+            <Heading
+              as="h5"
+              size="sm"
+              cursor="pointer"
+              onClick={() => navigate(`/profile/${comment.username}`)}
+            >
+              {comment.firstName} {comment.lastName}
+            </Heading>
+            <Text>{comment.commentData}</Text>
+          </Box>
+        </Flex>
+        {/* Edit Comment */}
+        {user.username === comment.username ? (
+          <Popover>
+            <PopoverTrigger>
+              <IconButton
+                icon={<BsThreeDotsVertical />}
+                bgColor="transparent"
+                color="black"
+                size="sm"
+                fontSize="lg"
+                _hover={{
+                  bgColor: "transparent",
+                }}
+                _active={{
+                  bgColor: "transparent",
+                  border: "none",
+                }}
+                _focus={{
+                  bgColor: "transparent",
+                  border: "none",
+                }}
+              ></IconButton>
+            </PopoverTrigger>
+            <PopoverContent maxW="fit-content">
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <PopoverBody>
+                <Button
+                  leftIcon={<FaRegEdit />}
+                  variant="ghost"
+                  display="block"
+                  onClick={callEditCommentHandler}
+                >
+                  Edit
+                </Button>
+                <Button
+                  leftIcon={<MdDelete />}
+                  variant="ghost"
+                  onClick={deleteCommentHandler}
+                >
+                  Delete
+                </Button>
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
+        ) : null}
+      </Flex>
+    </>
   );
 };
 
