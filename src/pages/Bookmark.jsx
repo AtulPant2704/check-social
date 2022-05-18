1a19420666de3fbd73bac1b6518451244fc23b13
AtulPant2704 <atulpant1999@gmail.com>
2022-05-18 20:32:22 +0530

    feat: display user bookmarks

---
+++ src/pages/Bookmark.jsx
@@ -0,0 +1,119 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Flex,
+  Box,
+  Heading,
+  CircularProgress,
+  useDisclosure,
+} from "@chakra-ui/react";
+import {
+  SideNav,
+  PostCard,
+  UsersSidebar,
+  MobileNav,
+  PostModal,
+} from "components";
+import { getPosts, getUserBookmarks } from "redux/asyncThunks";
+
+const Bookmark = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const dispatch = useDispatch();
+  const { bookmarks, token, bookmarkStatus } = useSelector(
+    (state) => state.auth
+  );
+  const { posts, status } = useSelector((state) => state.posts);
+  const [editedPost, setEditedPost] = useState(null);
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(getPosts());
+    }
+  }, [dispatch, status]);
+
+  useEffect(() => {
+    if (bookmarkStatus === "idle") {
+      dispatch(getUserBookmarks(token));
+    }
+  }, [dispatch, bookmarkStatus, token]);
+
+  const bookmarkedPosts = posts.filter((currPost) =>
+    bookmarks.some((curr) => curr === currPost._id)
+  );
+
+  return (
+    <>
+      <PostModal
+        isOpen={isOpen}
+        onClose={onClose}
+        editedPost={editedPost}
+        setEditedPost={setEditedPost}
+      />
+      <Box h="100%">
+        {status === "pending" ? (
+          <CircularProgress
+            isIndeterminate
+            color="brand.500"
+            position="fixed"
+            top="50%"
+            left="50%"
+            size="80px"
+            thickness="10px"
+          />
+        ) : null}
+        {status === "resolved" ? (
+          <>
+            <Flex justifyContent="center" bgColor="gray.200">
+              <Heading
+                color="brand.500"
+                display={{ base: "block", md: "none" }}
+              >
+                CheckSocial
+              </Heading>
+            </Flex>
+            <Flex backgroundColor="bg" w="90%" mx="auto" my="4" gap="10">
+              <SideNav onOpen={onOpen} />
+              {bookmarkedPosts.length !== 0 ? (
+                <Box
+                  maxW="40rem"
+                  minW={{ lg: "40rem", md: "70%", base: "100%" }}
+                  h="calc(95.2vh - 90px)"
+                >
+                  {bookmarkedPosts.map((post) => (
+                    <PostCard
+                      key={post._id}
+                      post={post}
+                      setEditedPost={setEditedPost}
+                      onOpen={onOpen}
+                    />
+                  ))}
+                </Box>
+              ) : (
+                <Flex w="50rem" justifyContent="center" alignItems="center">
+                  <Heading as="h3" size="md" textAlign="center">
+                    Nothing has been bookmarked.
+                  </Heading>
+                </Flex>
+              )}
+              <UsersSidebar />
+            </Flex>
+            <Box
+              position="sticky"
+              bottom="0"
+              left="0"
+              right="0"
+              h="50px"
+              bgColor="black"
+              display={{ base: "block", md: "none" }}
+              zIndex="2"
+            >
+              <MobileNav onOpen={onOpen} />
+            </Box>
+          </>
+        ) : null}
+      </Box>
+    </>
+  );
+};
+
+export { Bookmark };
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { Landing } from "./Landing";
 export { Home } from "./Home";
 export { Profile } from "./Profile";
 export { Explore } from "./Explore";
+export { Bookmark } from "./Bookmark";
