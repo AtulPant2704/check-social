fe6f28255b426bd6fa5d36495421a6f558ff79db
AtulPant2704 <atulpant1999@gmail.com>
2022-05-19 10:41:45 +0530

    refactor: removed loader from profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,7 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import {
-  Flex,
-  Box,
-  Heading,
-  CircularProgress,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Flex, Box, Heading, useDisclosure } from "@chakra-ui/react";
 import {
   SideNav,
   PostCard,
@@ -28,14 +22,13 @@ const Profile = () => {
   } = useDisclosure();
   const { username } = useParams();
   const { posts } = useSelector((state) => state.posts);
-  const [loader, setLoader] = useState(false);
   const [userProfile, setUserProfile] = useState(null);
   const [userPosts, setUserPosts] = useState(null);
   const [editedPost, setEditedPost] = useState(null);
 
   useEffect(() => {
-    getSingleUser(setUserProfile, username, setLoader);
-    getUserPosts(setUserPosts, username, setLoader);
+    getSingleUser(setUserProfile, username);
+    getUserPosts(setUserPosts, username);
   }, [username, posts]);
 
   return (
@@ -57,74 +50,60 @@ const Profile = () => {
         />
       )}
       <Box>
-        {loader ? (
-          <CircularProgress
-            isIndeterminate
+        <Flex justifyContent="center" bgColor="gray.200">
+          <Heading
+            position="sticky"
+            top="16px"
             color="brand.500"
-            position="fixed"
-            top="50%"
-            left="50%"
-            size="80px"
-            thickness="10px"
-          />
-        ) : (
-          <>
-            <Flex justifyContent="center" bgColor="gray.200">
-              <Heading
-                position="sticky"
-                top="16px"
-                color="brand.500"
-                display={{ base: "block", md: "none" }}
-              >
-                CheckSocial
-              </Heading>
-            </Flex>
-            <Flex backgroundColor="bg" w="90%" mx="auto" my="4" gap="10">
-              <SideNav onOpen={onOpen} />
-              <Box maxW="40rem">
-                <ProfileCard
-                  onOpenProfile={onOpenProfile}
-                  userProfile={userProfile}
-                  userpostsLength={userPosts?.length}
-                />
-                <Heading as="h3" size="md" mb="4">
-                  Your Posts
+            display={{ base: "block", md: "none" }}
+          >
+            CheckSocial
+          </Heading>
+        </Flex>
+        <Flex backgroundColor="bg" w="90%" mx="auto" my="4" gap="10">
+          <SideNav onOpen={onOpen} />
+          <Box maxW="40rem">
+            <ProfileCard
+              onOpenProfile={onOpenProfile}
+              userProfile={userProfile}
+              userpostsLength={userPosts?.length}
+            />
+            <Heading as="h3" size="md" mb="4">
+              Your Posts
+            </Heading>
+            {userPosts?.length > 0 ? (
+              [...userPosts]
+                ?.reverse()
+                .map((post) => (
+                  <PostCard
+                    key={post._id}
+                    post={post}
+                    setEditedPost={setEditedPost}
+                    onOpen={onOpen}
+                  />
+                ))
+            ) : (
+              <Box>
+                <Heading as="h3" size="md" textAlign="center">
+                  Nothing has been posted by your yet, Start posting and make
+                  friends.
                 </Heading>
-                {userPosts?.length > 0 ? (
-                  [...userPosts]
-                    ?.reverse()
-                    .map((post) => (
-                      <PostCard
-                        key={post._id}
-                        post={post}
-                        setEditedPost={setEditedPost}
-                        onOpen={onOpen}
-                      />
-                    ))
-                ) : (
-                  <Box>
-                    <Heading as="h3" size="md" textAlign="center">
-                      Nothing has been posted by your yet, Start posting and
-                      make friends.
-                    </Heading>
-                  </Box>
-                )}
               </Box>
-              <UsersSidebar />
-            </Flex>
-            <Box
-              position="sticky"
-              bottom="0"
-              left="0"
-              right="0"
-              bgColor="black"
-              display={{ base: "block", md: "none" }}
-              zIndex="2"
-            >
-              <MobileNav onOpen={onOpen} />
-            </Box>
-          </>
-        )}
+            )}
+          </Box>
+          <UsersSidebar />
+        </Flex>
+        <Box
+          position="sticky"
+          bottom="0"
+          left="0"
+          right="0"
+          bgColor="black"
+          display={{ base: "block", md: "none" }}
+          zIndex="2"
+        >
+          <MobileNav onOpen={onOpen} />
+        </Box>
       </Box>
     </>
   );
--- src/services/getUserPosts.js
+++ src/services/getUserPosts.js
@@ -1,14 +1,11 @@
 import axios from "axios";
 
-const getUserPosts = async (setUserPosts, username, setLoader) => {
+const getUserPosts = async (setUserPosts, username) => {
   try {
-    setLoader(true);
     const response = await axios.get(`/api/posts/user/${username}`);
     setUserPosts(response.data.posts);
   } catch (error) {
     console.error(error);
-  } finally {
-    setLoader(false);
   }
 };
 
