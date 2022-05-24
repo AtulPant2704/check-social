35cd04635b860c13204f16c870a31e4670449136
AtulPant2704 <atulpant1999@gmail.com>
2022-05-24 12:27:37 +0530

    feat: add debounce on search

--- src/components/SearchUser.jsx
+++ src/components/SearchUser.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import {
@@ -12,23 +12,27 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { ImSearch } from "react-icons/im";
-import { useEffect } from "react";
 
 const SearchUser = () => {
   const navigate = useNavigate();
   const [searchedUsers, setSearchedUsers] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const timerRef = useRef();
   const { users } = useSelector((state) => state.users);
 
   useEffect(() => {
-    const foundUsers = users.filter(
-      (user) =>
-        user.firstName.includes(searchQuery) ||
-        user.lastName.includes(searchQuery) ||
-        user.username.includes(searchQuery)
-    );
-    setSearchedUsers(foundUsers);
-  }, [users, searchQuery]);
+    clearTimeout(timerRef.current);
+    timerRef.current = setTimeout(() => {
+      const foundUsers = users.filter(
+        (user) =>
+          searchQuery.length !== 0 &&
+          (user.firstName.includes(searchQuery) ||
+            user.lastName.includes(searchQuery) ||
+            user.username.includes(searchQuery))
+      );
+      setSearchedUsers(foundUsers);
+    }, 1000);
+  }, [searchQuery, users]);
 
   return (
     <Box position="relative">
@@ -50,7 +54,7 @@ const SearchUser = () => {
           width="100%"
           zIndex="1"
           p="1"
-          borderColor="gray.50"
+          border="1px solid var(--chakra-colors-gray-200)"
           borderRadius="8"
         >
           {searchedUsers.map((user) => (
@@ -59,6 +63,7 @@ const SearchUser = () => {
               cursor="pointer"
               alignItems="center"
               gap="2"
+              px="2"
               onClick={() => navigate(`/profile/${user.username}`)}
             >
               <Avatar
